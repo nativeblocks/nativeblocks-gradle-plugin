10f21b7ac4cf207243e3c8e177bfd58c8ecae2b7
alireza <alireza@nativeblocks.io>
2024-08-09 23:19:11 +0300

    add okhttp client and send sync request for integration

--- gradle-plugin/src/main/java/io/nativeblocks/gradleplugin/integration/IntegrationRepository.kt
+++ gradle-plugin/src/main/java/io/nativeblocks/gradleplugin/integration/IntegrationRepository.kt
@@ -1,12 +1,41 @@
 package io.nativeblocks.gradleplugin.integration
 
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Optional
+import com.apollographql.apollo3.network.okHttpClient
 import io.nativeblocks.gradleplugin.GlobalState
+import io.nativeblocks.gradleplugin.network.AuthorizationInterceptor
+import io.nativeblocks.gradleplugin.network.NetworkRequestExecutor
+import io.nativeblocks.gradleplugin.network.execute
+import io.nativeblocks.network.SyncIntegrationMutation
+import io.nativeblocks.network.type.SyncIntegrationInput
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.intOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import okhttp3.OkHttpClient
 import org.gradle.api.GradleException
 import org.gradle.api.Project
-
+import java.util.concurrent.TimeUnit
 
 class IntegrationRepository {
+
+    private val authorizationInterceptor = AuthorizationInterceptor(GlobalState.authToken.orEmpty())
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authorizationInterceptor)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    private val apolloClient = ApolloClient.Builder()
+        .httpServerUrl(GlobalState.endpoint.orEmpty())
+        .okHttpClient(okHttpClient)
+        .build()
+
+    private val networkRequestExecutor = NetworkRequestExecutor()
 
     suspend fun syncIntegration(project: Project, kind: String) {
         val directoryPath =
@@ -55,6 +84,38 @@ class IntegrationRepository {
             }
 
             IntegrationMeta(integrationJson, propertiesJson, eventsJson, dataJson, slotsJson)
+        }
+
+        integrationJsonList.forEach { meta ->
+            if (meta.integrationJson == null) return
+
+            val request = apolloClient.mutation(
+                SyncIntegrationMutation(
+                    input = SyncIntegrationInput(
+                        name = meta.integrationJson.jsonObject["name"]?.jsonPrimitive?.content.orEmpty(),
+                        keyType = meta.integrationJson.jsonObject["keyType"]?.jsonPrimitive?.content.orEmpty(),
+                        imageIcon = Optional.presentIfNotNull(meta.integrationJson.jsonObject["imageIcon"]?.jsonPrimitive?.content.orEmpty()),
+                        price = meta.integrationJson.jsonObject["price"]?.jsonPrimitive?.intOrNull ?: 0,
+                        description = Optional.presentIfNotNull(meta.integrationJson.jsonObject["description"]?.jsonPrimitive?.content.orEmpty()),
+                        documentation = Optional.presentIfNotNull(meta.integrationJson.jsonObject["documentation"]?.jsonPrimitive?.content.orEmpty()),
+                        platformSupport = meta.integrationJson.jsonObject["platformSupport"]?.jsonPrimitive?.content.orEmpty(),
+                        public = meta.integrationJson.jsonObject["public"]?.jsonPrimitive?.booleanOrNull ?: false,
+                        kind = meta.integrationJson.jsonObject["kind"]?.jsonPrimitive?.content.orEmpty(),
+                        organizationId = GlobalState.organizationId.orEmpty()
+                    )
+                )
+            )
+
+            networkRequestExecutor.requestExecutor(request).execute({ res ->
+                runBlocking {
+                    val id = res?.syncIntegration?.id.orEmpty()
+                    val name = res?.syncIntegration?.name.orEmpty()
+
+                    // sync meta
+                }
+            }, { error ->
+                throw GradleException("The ${meta.integrationJson.jsonObject["name"]?.jsonPrimitive?.content.orEmpty()} failed to upload because: ${error.message}")
+            })
         }
     }
 }
